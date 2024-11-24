5f05932ec43c979c1bb5dbd77d52b2862903b0bc
cjavier28 <86753347+cjavier28@users.noreply.github.com>
2024-11-24 14:23:49 -0500

    Se crea rama master proyecto

---
+++ DOTNET_Servicio_Gestion_Eventos_Asistentes/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DOTNET_Servicio_Gestion_Eventos_Asistentes
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
